c4c9418bda1c8c56410506bcd13485eb3395c6f1
E <79379754+oech3@users.noreply.github.com>
2025-07-11 02:54:10 +0900

    zsh

--- build-linux.sh
+++ build-linux.sh
@@ -1,7 +1,7 @@
-#!/bin/bash -e
+#!/bin/zsh -e
 # https://chromium.googlesource.com/chromium/third_party/ffmpeg/+/refs/heads/master/
 # See BUILD.gn and chromium/config/Chrome/linux/x64/
-declare -A ffbuildflags=(
+typeset -A ffbuildflags=(
 [linux-x64]=
 [linux-ia32]='--arch=x86 --target-os=linux --cpu=x86 --enable-cross-compile'
 [osx-x64]='--arch=x86_64 --target-os=darwin --cpu=x86_64'
@@ -9,7 +9,7 @@ declare -A ffbuildflags=(
 [win-x64]='--arch=x86_64 --target-os=mingw32 --cross-prefix=x86_64-w64-mingw32-'
 [win-ia32]='--arch=x86 --target-os=mingw32 --cross-prefix=i686-w64-mingw32-'
 )
-declare -A extcflags=(
+typeset -A extcflags=(
 [linux-x64]='-fno-math-errno -fno-signed-zeros'
 [linux-ia32]='-m32 -fno-math-errno -fno-signed-zeros'
 [osx-x64]=
@@ -17,7 +17,7 @@ declare -A extcflags=(
 [win-x64]=
 [win-ia32]=
 )
-declare -A extldflags=(
+typeset -A extldflags=(
 [linux-x64]='-Wl,-O1 -Wl,--sort-common -Wl,--as-needed -Wl,-z,relro -Wl,-z,now -Wl,-z,pack-relative-relocs'
 [linux-ia32]='-Wl,-O1 -Wl,--sort-common -Wl,--as-needed -Wl,-z,relro -Wl,-z,now -Wl,-z,pack-relative-relocs'
 [osx-x64]=
@@ -57,7 +57,7 @@ mv -f libavcodec/opus/dec.c.patched libavcodec/opus/dec.c
   make -j3 install
 
 cd ../release
-declare -A cc=(
+typeset -A cc=(
 [linux-x64]=gcc
 [linux-ia32]='gcc -m32'
 [osx-x64]=clang
@@ -65,7 +65,7 @@ declare -A cc=(
 [win-x64]=x86_64-w64-mingw32-gcc
 [win-ia32]=i686-w64-mingw32-gcc
 )
-declare -A strip=(
+typeset -A strip=(
 [linux-x64]='strip --strip-unneeded'
 [linux-ia32]='strip --strip-unneeded'
 [osx-x64]='strip -x'
@@ -73,7 +73,7 @@ declare -A strip=(
 [win-x64]='x86_64-w64-mingw32-strip --strip-unneeded'
 [win-ia32]='i686-w64-mingw32-strip --strip-unneeded'
 )
-declare -A gccflag=(
+typeset -A gccflag=(
 [linux-x64]='-Wl,-u,avutil_version -lm -Wl,-Bsymbolic'
 [linux-ia32]='-Wl,-u,avutil_version -lm -Wl,-Bsymbolic'
 [osx-x64]=
@@ -81,7 +81,7 @@ declare -A gccflag=(
 [win-x64]='-lbcrypt'
 [win-ia32]='-lbcrypt'
 )
-declare -A ldwholearchive=(
+typeset -A ldwholearchive=(
 [linux-x64]='-Wl,--whole-archive '
 [linux-ia32]='-Wl,--whole-archive '
 [osx-x64]='-Wl,-force_load,'
@@ -89,7 +89,7 @@ declare -A ldwholearchive=(
 [win-x64]='-Wl,--whole-archive '
 [win-ia32]='-Wl,--whole-archive '
 )
-declare -A ldnowholearchive=(
+typeset -A ldnowholearchive=(
 [linux-x64]='-Wl,--no-whole-archive '
 [linux-ia32]='-Wl,--no-whole-archive '
 [osx-x64]=
@@ -97,7 +97,7 @@ declare -A ldnowholearchive=(
 [win-x64]='-Wl,--no-whole-archive '
 [win-ia32]='-Wl,--no-whole-archive '
 )
-declare -A libext=(
+typeset -A libext=(
 [linux-x64]=so
 [linux-ia32]=so
 [osx-x64]=dylib
